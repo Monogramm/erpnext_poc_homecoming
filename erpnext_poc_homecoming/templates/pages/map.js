2485a43060c0bd760b02a38e5ef5169e339e23c1
Emil <amionv99@gmail.com>
2020-04-23 21:33:39 +0300

    :sparkles: Add template for every doctype
    Signed-off-by: Emil <amionv99@gmail.com>

--- erpnext_poc_homecoming/templates/pages/map.js
+++ erpnext_poc_homecoming/templates/pages/map.js
@@ -5,18 +5,28 @@ L.tileLayer("https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=FaOWvrcTP
         '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
 }).addTo(map);
 
+let current_url = window.location.href.split('/');
+let let_url = current_url[0] + "//" + current_url[1] + current_url[2] +
+    "/api/method/erpnext_poc_homecoming.erpnext_poc_homecoming.doctype.organization.organization.get_list_map";
 
-frappe.call({
-    method: "erpnext_poc_homecoming.erpnext_poc_homecoming.doctype.organization.organization.get_list_map",
-    callback: function (r) {
+window.onload = function () {
+    $.ajax({
+    datatype: "json",
+    beforeSend: function (xhr) {
+        xhr.setRequestHeader('X-Frappe-CSRF-Token', frappe.csrf_token);
+    },
+    url: let_url,
+    success: function (r) {
         let places = r.message;
         console.log(places.length);
         console.log(places);
         for (let i = 0; i < places.length; i++) {
             let place = JSON.parse(places[i].location);
             L.marker(place.features[0].geometry.coordinates.reverse()).addTo(map)
-                .bindPopup('<a href="'+ places[i].route + '">' + places[i].title + '</a>')
+                .bindPopup('<a href="' + places[i].route + '">' + places[i].title + '</a>')
                 .openPopup();
         }
-    }
+    },
 });
+
+};
